c182d738f85f89da35a923d843c769da40867f9c
yu840915 <yu840915@gmail.com>
2021-03-30 00:36:15 +0800

    Parse business hours

--- functions/gym/editGym.js
+++ functions/gym/editGym.js
@@ -14,7 +14,14 @@ const { createClientError } = require('../clientError');
  * @param {import('./gym').Gym} gymInfo
  */
 const createGym = async (gymInfo) => {
-  const { id = null, address, equipments, phones = [], pricing } = gymInfo;
+  const {
+    id = null,
+    address,
+    equipments,
+    phones = [],
+    pricing,
+    businessHours,
+  } = gymInfo;
   const gymRef = id ? gymsRef.doc(id) : gymsRef.doc();
   const gymSnap = await gymRef.get();
   if (gymSnap.exists) {
@@ -48,6 +55,7 @@ const createGym = async (gymInfo) => {
     townId,
     countyId,
     hourlyRate: lowestHourlyRate(pricing),
+    businessHours: parseBusinessHours(businessHours),
   };
   try {
     await gymRef.create(data);
@@ -57,6 +65,49 @@ const createGym = async (gymInfo) => {
     }
     throw error;
   }
+};
+
+/**
+ * @param {[import('./gym').BusinessHours]} businessHours
+ */
+const parseBusinessHours = (businessHours) => {
+  if (!businessHours || businessHours.length === 0) {
+    return null;
+  }
+  /**
+   * @type {import('./gym').BusinessHours}
+   */
+  let base;
+  const specialCases = {};
+
+  for (const descriptor of businessHours) {
+    if (!descriptor.dayOfWeek) {
+      base = descriptor;
+    } else {
+      specialCases[descriptor.dayOfWeek.toLowerCase()] = descriptor;
+    }
+  }
+
+  if (!base && Object.keys(specialCases).length !== 7) {
+    throw createClientError(
+      400,
+      'Please specify base case or business hours for every weekday'
+    );
+  }
+
+  const weekdays = ['mon', 'tue', 'wed', 'thu', 'fir', 'sat', 'sun'];
+  /**
+   * @type {[import('./gym').BusinessHours]}
+   */
+  const retVals = weekdays.map((e) => {
+    const val = specialCases[e] ?? base;
+    /**
+     * @type {import('./gym').BusinessHours}
+     */
+    const ret = { ...val, dayOfWeek: e };
+    return ret;
+  });
+  return retVals;
 };
 
 /**
