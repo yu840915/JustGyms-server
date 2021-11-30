824a94f512b762548d667d644f625b1d792a896d
yu840915 <yu840915@gmail.com>
2021-11-30 23:56:32 +0800

    Role checker

--- functions/admin/user/routes.js
+++ functions/admin/user/routes.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const { asyncRequestHandler } = require('../../firebaseFunctions');
 const app = express();
+const { authenticate } = require('../../authenticate');
 const { addUserRoles, removeUserRoles } = require('./roles');
 const validationRules = require('./validationRules');
 const validator = require('express-joi-validation').createValidator();
+const RoleChecker = require('../../roleChecker');
+
+const requireAdmin = new RoleChecker([Roles.admin]).checkRolesMiddleware;
 
 app.put(
   '/:userId/roles',
   validator.params(validationRules.userId),
   validator.query(validationRules.roles),
+  authenticate,
+  requireAdmin,
   asyncRequestHandler(async (req, res) => {
     const { val: roles } = req.query;
     console.log(roles);
@@ -21,6 +27,8 @@ app.delete(
   '/:userId/roles',
   validator.params(validationRules.userId),
   validator.query(validationRules.roles),
+  authenticate,
+  requireAdmin,
   asyncRequestHandler(async (req, res) => {
     const { val: roles } = req.query;
     await removeUserRoles({ userId: req.params.userId, roles });
